c5a80d8138f1dd5fa8742d7a63bfa007b50d9233
Jay Lloyd Chicogo <jaychicogo328@gmail.com>
2026-04-12 20:58:05 +0800

    Added get groups endpoint and join group functionality (#10)

--- backend/src/modules/routes/group.router.js
+++ backend/src/modules/routes/group.router.js
@@ -1,18 +1,31 @@
 const express = require("express");
 
-const { createGroup, joinGroup } = require("../controllers/group.controller");
+const {
+  createGroup,
+  joinGroup,
+  getGroups,
+} = require("../controllers/group.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const groupRouter = express.Router();
 
-groupRouter.get("/group", (req, res) => {
-  res.send({ message: "You are on a group" });
+groupRouter.get("/group", authMiddleware, async (req, res) => {
+  const { id } = req.student;
+  try {
+    const groups = await getGroups(id);
+
+    res.status(200).json({ groups });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // CREATE GROUP
-groupRouter.post("/group/create", async (req, res) => {
+groupRouter.post("/group/create", authMiddleware, async (req, res) => {
   const group = req.body;
+  const { id } = req.student;
   try {
-    const newGroup = await createGroup(group);
+    const newGroup = await createGroup(group, id);
 
     res.status(201).json(newGroup);
   } catch (error) {
@@ -21,13 +34,14 @@ groupRouter.post("/group/create", async (req, res) => {
 });
 
 // JOIN GROUP
-groupRouter.post("/group/join", async (req, res) => {
-  const { membership } = req.body;
+groupRouter.post("/group/join", authMiddleware, async (req, res) => {
+  const { code } = req.body;
+  const { id } = req.student;
   try {
-    const member = await joinGroup(membership);
+    const member = await joinGroup(code, id);
     res.status(200).json({ member });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
