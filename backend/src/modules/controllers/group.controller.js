4b58e7daeab08c2d1e30744c500541982b8cdf04
jayl-ch <jaychicogo328@gmail.com>
2026-04-12 21:10:56 +0800

    feat: add controller logic for students to join a group

--- backend/src/modules/controllers/group.controller.js
+++ backend/src/modules/controllers/group.controller.js
@@ -1,12 +1,34 @@
 const Group = require("../models/group.model");
 const GroupMember = require("../models/group.member.model");
+const mongoose = require("mongoose");
 
-const createGroup = async (group) => {
-  return await Group.create(group);
+const createGroup = async (group, id) => {
+  return await Group.create({
+    ...group,
+    owner: new mongoose.Types.ObjectId(id),
+  });
 };
 
-const joinGroup = async (membership) => {
-  return await GroupMember.create(membership);
+const joinGroup = async (code, id) => {
+  const group = await Group.findOne({ inviteCode: code });
+  if (!group) throw new Error("Invalid code");
+
+  const existing = await GroupMember.findOne({
+    groupId: group._id,
+    studentId: id,
+  });
+
+  if (existing) throw new Error("Already joined");
+
+  return await GroupMember.create({
+    groupId: group._id,
+    studentId: id,
+    role: "member",
+  });
 };
 
-module.exports = { createGroup, joinGroup };
+const getGroups = async (id) => {
+  return await Group.find({ owner: new mongoose.Types.ObjectId(id) });
+};
+
+module.exports = { createGroup, joinGroup, getGroups };
