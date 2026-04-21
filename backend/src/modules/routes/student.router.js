89a89fc1ad76d6bba51e45d320370fa2d1a7beee
jayl-ch <jaychicogo328@gmail.com>
2026-04-21 18:01:47 +0800

    feat: wired validators into mutation/auth routes

--- backend/src/modules/routes/student.router.js
+++ backend/src/modules/routes/student.router.js
@@ -7,20 +7,48 @@ const {
   createStudent,
   findStudent,
   updateAuthStudent,
+  refreshSession,
+  logoutStudent,
 } = require("../controllers/student.controller");
 
 const authMiddleware = require("../middlewares/auth.middleware");
+const validateRequest = require("../middlewares/validate.middleware");
+const {
+  validateRegister,
+  validateLogin,
+  validateRefreshPayload,
+  validateProfileUpdate,
+} = require("../validators/request.validators");
 
 // GET AUTHENTICATED STUDENT
 studentRouter.get("/student", authMiddleware, getAuthStudent);
 
 // UPDATE AUTHENTICATED STUDENT PROFILE
-studentRouter.patch("/student", authMiddleware, updateAuthStudent);
+studentRouter.patch(
+  "/student",
+  authMiddleware,
+  validateRequest(validateProfileUpdate),
+  updateAuthStudent,
+);
 
 // REGISTER
-studentRouter.post("/student/register", createStudent);
+studentRouter.post("/student/register", validateRequest(validateRegister), createStudent);
 
 // LOGIN
-studentRouter.post("/student/login", findStudent);
+studentRouter.post("/student/login", validateRequest(validateLogin), findStudent);
+
+// REFRESH SESSION
+studentRouter.post(
+  "/student/refresh",
+  validateRequest(validateRefreshPayload),
+  refreshSession,
+);
+
+// LOGOUT
+studentRouter.post(
+  "/student/logout",
+  validateRequest(validateRefreshPayload),
+  logoutStudent,
+);
 
 module.exports = studentRouter;
