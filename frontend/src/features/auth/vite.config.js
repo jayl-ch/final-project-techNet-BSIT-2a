b03cb68b5120aaf47bab0a8243decbc6631a6206
Sannie-dev <christiansanniesaelsilo@gmail.com>
2026-04-22 21:42:05 +0800

    feat: add auth page

---
+++ frontend/src/features/auth/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  const apiTarget = env.SERVER;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: apiTarget,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
