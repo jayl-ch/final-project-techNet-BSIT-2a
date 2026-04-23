75e17f5ffb48ca6190ffc12b593bd10b0f103e05
Sannie-dev <christiansanniesaelsilo@gmail.com>
2026-04-24 00:35:57 +0800

    move files using file manager

--- frontend/src/app/AppRouter.jsx
+++ frontend/src/app/AppRouter.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "../features/landing";
+import { LoginPage, RegisterPage } from "../features/auth";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 };
