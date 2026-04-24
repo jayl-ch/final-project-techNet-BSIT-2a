3b7e9cc2833ecc8de7efa96b98f849304bf66867
Christian <christiansanniesaelsilo@gmail.com>
2026-04-24 08:54:02 +0800

    Feature/frontend auth pages (#32)
    * feat: add auth page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  const apiTarget = env.SERVER;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: apiTarget,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
