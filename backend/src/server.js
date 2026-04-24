7c8d7dcdfffeee1621917e915cc818b5c654e9a9
Christian <christiansanniesaelsilo@gmail.com>
2026-04-24 20:45:42 +0800

    refactor: revert the local mongo to production

--- backend/src/server.js
+++ backend/src/server.js
@@ -50,7 +50,7 @@ app.use(errorHandler);
 
 async function start() {
   const port = process.env.PORT;
-  const uri = process.env.MONGO_URI_LOCAL;
+  const uri = process.env.MONGO_URI;
 
   await connectDB(uri);
 
