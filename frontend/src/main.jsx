d9f5181378b1bfcbcf72bfabf88ff02b8363b55b
Christian <christiansanniesaelsilo@gmail.com>
2026-04-26 15:37:05 +0800

    Implement persistent dark theme with custom Bootstrap styles (#34)
    * feat: implement dark theme with persistence
    
    * feat: override Bootstrap dark theme palette with modern styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./app/styles/index.css";
 import App from "./app/App";
 import { BrowserRouter } from "react-router";
+import { ThemeProvider } from "./app/providers/theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 );
