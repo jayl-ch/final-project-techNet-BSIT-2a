039cb286ef43033ed4551ffffb1266f882a76b01
Christian <christiansanniesaelsilo@gmail.com>
2026-05-12 15:52:02 +0800

    feat: add DeveloperRoute component for developer access control

--- frontend/src/features/auth/components/index.js
+++ frontend/src/features/auth/components/index.js
@@ -3,3 +3,4 @@ export { default as AuthLogin } from "./AuthLogin";
 export { default as AuthRegister } from "./AuthRegister";
 export { default as ProtectedRoute } from "./ProtectedRoute";
 export { default as PublicRoute } from "./PublicRoute";
+export { default as DeveloperRoute } from "./DeveloperRoute";
